69300384db72fd0c18beacdf2f448ff3eee6c5af
pixelbuildlab <2017n2801@gmail.com>
2026-08-05 22:06:45 +0500

    setup git deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: process.env.GITHUB_ACTIONS && repositoryName ? `/${repositoryName}/` : '/',
   server: {
     host: '::',
     port: 8080,
